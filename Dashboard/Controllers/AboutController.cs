95c0257be87a530e5f25e78e4251e2ce3c516a74
NoOverflow <julien.prost@epitech.eu>
2021-11-23 20:52:41 +0100

    Remove useless controller entry

--- Dashboard/Controllers/AboutController.cs
+++ Dashboard/Controllers/AboutController.cs
@@ -1,15 +1,12 @@
 ﻿using Dashboard.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Dashboard.Controllers
 {
     [Route("about.json")]
     [ApiController]
     public class AboutController : ControllerBase
     {
-        // GET: api/<AboutController>
         [HttpGet]
         public ObjectResult Get()
         {
@@ -25,31 +22,6 @@ namespace Dashboard.Controllers
                 HostIp = HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString(),
             };
             return Ok(about);
-        }
-
-        // GET api/<AboutController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<AboutController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<AboutController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AboutController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
