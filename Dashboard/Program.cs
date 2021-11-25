12e545668dd1535bcf629e6108b4f9dfd8d2951d
NoOverflow <julien.prost@epitech.eu>
2021-11-25 23:53:09 +0100

    Integrate entity framework

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,16 +1,23 @@
+using Dashboard.Areas.Identity.Data;
 using Dashboard.Data;
 using Dashboard.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("DashboardContextConnection");builder.Services.AddDbContext<DashboardContext>(options =>
+    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<DashboardUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DashboardContext>();
 // Add services to the container.
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<TestService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 var app = builder.Build();
 
 
@@ -22,6 +29,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseAuthentication();
 app.UseHttpsRedirection();
 app.UseMvcWithDefaultRoute();
 app.UseStaticFiles();
