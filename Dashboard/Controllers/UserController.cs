68a5376948a8888cd92b36b34433621243aa1af3
NoOverflow <julien.prost@epitech.eu>
2021-12-01 20:44:08 +0100

    Improve logout user experience

---
+++ Dashboard/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Dashboard.Areas.Identity.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly SignInManager<DashboardUser> _signInManager;
+
+        public UserController(SignInManager<DashboardUser> signInManager)
+        {
+            this._signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<ActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
